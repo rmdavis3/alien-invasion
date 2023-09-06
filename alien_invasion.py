e0bb5f631c43f425ffd506dac5983cd91da4490d
Ryan Davis <94387716+sdl1wrkhremb@users.noreply.github.com>
2023-09-06 11:25:43 -0400

    removed duplicate line

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,7 +15,6 @@ class AlienInvasion:
         # pylint: enable=no-member
         self.settings = Settings()
         self.clock = pygame.time.Clock()
-        self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
