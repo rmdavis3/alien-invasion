9a1673b73a32e892ffd92e58f6ce958272b007d1
rmdavis3 <94387716+rmdavis3@users.noreply.github.com>
2024-12-25 07:04:47 -0500

    deactivated play button

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -78,7 +78,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
             self.stats.reset_stats()
             self.game_active = True
 
@@ -153,7 +154,7 @@ class AlienInvasion:
             self.ship.center_ship()
 
             # Pause.
-            sleep(1.0)
+            sleep(0.5)
         else:
             self.game_active = False
 
