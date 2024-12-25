d1ea04acc56a96de22fe5a43efc8023b6007be9c
rmdavis3 <94387716+rmdavis3@users.noreply.github.com>
2024-12-25 07:01:08 -0500

    reset game for play button

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -32,7 +32,7 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings.
-        self.alien_speed = 5.0
+        self.alien_speed = 50.0
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
