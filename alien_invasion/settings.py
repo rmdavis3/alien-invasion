69d6f61bd5fb6e8a06ade1baec8d935c0a22f4b4
rmdavis3 <94387716+rmdavis3@users.noreply.github.com>
2024-12-26 06:27:28 -0500

    reset game settings

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -33,7 +33,7 @@ class Settings:
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up.
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.2
 
         self.initialize_dynamic_settings()
 
